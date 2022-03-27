f6a95427b5c54b158558bad2127d14329330a9cf
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-03-27 19:49:56 +0100

    Created MongoDB Atlas collection and connected our program to it.

--- server/app.js
+++ server/app.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 5000
+const {MONGOURI} = require('./keys')
 
-const customMiddleware = (req, res, next) => {
-    console.log("middleware executed!!")
-    next()
-}
-
-
-app.get('/',(req, res) => {
-    console.log("home")
-    res.send("hello world")
+mongoose.connect(MONGOURI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongo")
+})
+mongoose.connection.on('error', (err) => {
+    console.log("error connecting", err)
 })
 
-app.get('/about', customMiddleware, (req, res) => {
-    console.log("about")
-    res.send("about page")
-})
 
 app.listen(PORT,() => {
     console.log("server is running on", PORT)
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://mongodb_atlas_user_1:5cilecd5Qsrs9Lal@cluster0.lol7h.mongodb.net/test?retryWrites=true&w=majority"
+}
